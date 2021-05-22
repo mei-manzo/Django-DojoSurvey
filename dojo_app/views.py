26d8bdad22d079fa0c213e177ac3b63e43ba6095
Mei Manzo <mmanzo806@gmail.com>
2021-05-21 23:43:33 -0700

    completed coding out, can still touch up css

--- dojo_app/views.py
+++ dojo_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 def index(request):
     return render(request, "index.html")
+
+def some_function(request):
+    if request.method == "POST":
+        context = {
+            'name': request.POST['name'],
+            'language': request.POST['language'],
+            'location': request.POST['location'],
+            'gender': request.POST['gender'],
+            'description': request.POST['description'],
+        }
+        return render(request, "other.html", context)
+    return render(request, "other.html")
