493684b8d3a0054469eca4266525f8624614aeed
Mei Manzo <mmanzo806@gmail.com>
2021-05-22 21:38:39 -0700

    first coding out

--- dojo_app/urls.py
+++ dojo_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('result', views.some_function),
+    path('this_function', views.this_function),
 ]
--- dojo_app/views.py
+++ dojo_app/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render, redirect, HttpResponse
+
 def index(request):
+    print('got here from redirect') #start here, post to /result (some_function), redirect to this_function
     return render(request, "index.html")
 
 def some_function(request):
     if request.method == "POST":
         context = {
             'name': request.POST['name'],
-            'language': request.POST['language'],
-            'location': request.POST['location'],
-            'gender': request.POST['gender'],
-            'description': request.POST['description'],
+            # 'language': request.POST['language'],
+            # 'location': request.POST['location'],
+            # 'gender': request.POST['gender'],
+            # 'description': request.POST['description'],
         }
-        return render(request, "other.html", context)
+        # return render(request, "other.html", context)
+        return redirect('this_function')
+    return redirect('this_function')
+
+def this_function(request):
+    request.session['name'] = request.POST['name']
+    request.session['language'] = request.POST['language']
+    request.session['location'] = request.POST['location']
+    request.session['gender'] = request.POST['gender']
+    request.session['description'] = request.POST['description']
+    request.session['counter'] = 100
     return render(request, "other.html")
+    
+    
+
