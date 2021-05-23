93f53033a75c17158ba23ba5ac416a56678bf0b8
Mei Manzo <mmanzo806@gmail.com>
2021-05-23 13:52:03 -0700

    finalized code

--- dojo_app/urls.py
+++ dojo_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('result', views.some_function),
-    path('this_function', views.this_function),
+    path('some_function', views.some_function),
+    path('result', views.result),
 ]
